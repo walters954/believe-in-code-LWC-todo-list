378285890c97e77ef281fd4eb7feaf33850242e8
Warren Walters <warren.salesforce@gmail.com>
2023-04-10 20:12:40 -0500

    complete

--- force-app/main/default/lwc/todoList/todoList.js
+++ force-app/main/default/lwc/todoList/todoList.js
@@ -3,7 +3,12 @@ import { LightningElement } from 'lwc';
 export default class TodoList extends LightningElement {
 
     todoValue = 'test';
-    todoList = [];
+    todoList = [{
+        id: 1,
+        value: 'Get Milk'
+    }];
+
+    idCounter = 0;
 
     get hasTodoListItems(){
         return this.todoList.length > 0;
@@ -11,8 +16,9 @@ export default class TodoList extends LightningElement {
 
     handleClick() {
         console.log('We have clicked the add button', this.todoList);
+        this.idCounter ++;
         let newTodo = {
-            id : this.todoList.length + 1,
+            id : this.idCounter,
             value : this.todoValue
         }
         this.todoList = [...this.todoList, newTodo];
@@ -22,5 +28,21 @@ export default class TodoList extends LightningElement {
     handleChange(event) {
         console.log('We have changed a value on the input' , event.target.value);
         this.todoValue = event.target.value;
+    }
+
+    deleteHandler(event) {
+        console.log('deleteHandler from the parent', event.detail);
+        const childTodoId = event.detail;
+        this.todoList = this.todoList.filter(todo => todo.id !== childTodoId);
+    }
+
+    connectedCallback() {
+        console.log('connectCallback', this.idCounter);
+        this.idCounter = this.todoList.length;
+    }
+
+    handleClear() {
+        console.log('handleClear');
+        this.todoList = [];
     }
 }
--- force-app/main/default/lwc/todoListItem/todoListItem.js
+++ force-app/main/default/lwc/todoListItem/todoListItem.js
@@ -3,6 +3,7 @@ import { LightningElement, api } from 'lwc';
 export default class TodoListItem extends LightningElement {
 
     @api todoValue;
+    @api todoId;
     todoStatus = false;
     
     get isCompleted() {
@@ -12,5 +13,14 @@ export default class TodoListItem extends LightningElement {
     handleComplete() {
         console.log('handleComplete current status: ', this.todoStatus);
         this.todoStatus = !this.todoStatus;
+    }
+
+    handleDelete() {
+        console.log('handleDelete from child', this.todoId);
+        const deleteEvent = new CustomEvent('delete', {
+            detail: this.todoId
+        });
+        this.dispatchEvent(deleteEvent);
+
     }
 }
